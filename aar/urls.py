10a5895a58e79bffccd0d9e5a857617595b20ea0
Jeferson Luis <jgjefersonluis@gmail.com>
2021-04-28 20:01:01 -0300

    ulrs projeto e apps

--- aar/urls.py
+++ aar/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('', include('charts.urls')),
+    path('', include('blogs.urls')),
+    path('', include('services.urls')),
+    path('', include('tasks.urls')),
+
 ]
