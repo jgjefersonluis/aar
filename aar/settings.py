aff1a6612153500b288193f8a28547d03b75897e
Jeferson Luis <jgjefersonluis@gmail.com>
2021-04-28 19:56:12 -0300

    instalado as bibliotecas bootstrap e crispyforms

--- aar/settings.py
+++ aar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--1%37r$qye1$kca%c_7dfj3k$w(o%2=l*=j)02cjb&smm_e5-_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'tasks',
+    'charts',
+    'blogs',
+    'services',
+    'bootstrap4',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
