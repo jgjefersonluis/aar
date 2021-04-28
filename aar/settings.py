cb526902a48ab69dbd2fae6a25a84efc5eb7ce38
Jeferson Luis <jgjefersonluis@gmail.com>
2021-04-28 19:57:36 -0300

    configurado settings.py os apps e templates

--- aar/settings.py
+++ aar/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'aar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
